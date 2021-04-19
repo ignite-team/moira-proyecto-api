241d2b468ac92bb2195225913ca43c9e2f82218b
Dario Aldrey Muiño <a19darioam@iessanclemente.net>
2021-04-19 12:23:10 +0200

    init

---
+++ src/main/java/es/ozona/moira/proyecto/api/Application.java
@@ -0,0 +1,13 @@
+package es.ozona.moira.proyecto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
